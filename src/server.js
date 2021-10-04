7c19f375eec05eefa7448df0ff1ca9755425d393
André Muniz <andre250899@gmail.com>
2021-10-04 13:40:46 -0300

    add more routes and job features

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const server = express();
 const routes = require("./routes");
+const path = require('path')
 
 //template engine do ejs
 server.set('view engine', 'ejs')
 
+//mudar a localização da pasta views
+server.set('views', path.join(__dirname, 'views'))
+
 //habilitar arquivos statics
 server.use(express.static('public'))
+
+//usar o req.body
+server.use(express.urlencoded({ extended: true }))
 
 //usando as routes
 server.use(routes)
