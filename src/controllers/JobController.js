c30a5b406c51f615eae2aa12092f4beed77ceaa0
André Muniz <andre250899@gmail.com>
2021-10-05 00:01:00 -0300

    added database

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -10,12 +10,8 @@ module.exports = {
             
     },
 
-    save(req, res) {
-        const jobs = Job.get();
-        const lastId = jobs[jobs.length - 1]?.id || 0;
-    
-        Job.create({
-            id: lastId + 1,
+    async save(req, res) {
+        await Job.create({
             name: req.body.name,
             'daily-hours': req.body['daily-hours'], 
             'total-hours': req.body['total-hours'],
@@ -26,9 +22,9 @@ module.exports = {
             
     },
 
-    show(req, res) {
-        const jobs = Job.get();
-        const profile = Profile.get();
+    async show(req, res) {
+        const jobs = await Job.get();
+        const profile = await Profile.get();
         const jobId = req.params.id;
 
         const job = jobs.find(job => Number(job.id) === Number(jobId))
@@ -42,41 +38,24 @@ module.exports = {
         return res.render("job-edit", { job })
     },
 
-    update(req, res) {
-        const jobs = Job.get();
+    async update(req, res) {
         const jobId = req.params.id
 
-        const job = jobs.find(job => Number(job.id) === Number(jobId))
-
-        if (!job) {
-            return res.send('Job not found!')
-        }
-
-        const updateJob = {
-            ...job,
+        const updatedJob = {
             name: req.body.name,
             'total-hours': req.body['total-hours'],
             'daily-hours': req.body['daily-hours']
         }
 
-        const newJobs = jobs.map(job => {
+        await Job.update(updatedJob, jobId)
 
-            if (Number(job.id) === Number(jobId)) {
-                job = updateJob
-            }
-
-            return job
-        })
-
-        Job.update(newJobs)
-
-        return res.redirect('/job/' + jobId)
+        res.redirect('/job/' + jobId)
     },
 
-    delete(req, res) {
+    async delete(req, res) {
         const jobId = req.params.id
 
-        Job.delete(jobId)
+        await Job.delete(jobId)
 
         return res.redirect('/')
     }
